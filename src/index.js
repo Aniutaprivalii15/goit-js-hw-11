853812250e8db568af21656a6a13a632c1df0f58
Anna <anytka141528@gmail.com>
2023-02-23 17:04:34 +0200

    hm-11

--- src/index.js
+++ src/index.js
@@ -1,45 +1,55 @@
 import('./css/styles.css');
-import { fetchImages } from './js/fetch-images'
-import { renderGallery } from './js/render-gallery'
-import { onScroll, onToTopBtn } from './js/scroll'
-import Notiflix from 'notiflix'
-import SimpleLightbox from 'simplelightbox'
-import 'simplelightbox/dist/simple-lightbox.min.css'
+import { fetchImages } from './js/fetch-images';
+import { renderGallery } from './js/render-gallery';
+import { onScroll, onToTopBtn } from './js/scroll';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const searchForm = document.querySelector('#search-form')
-const gallery = document.querySelector('.gallery')
-const loadMoreBtn = document.querySelector('.btn-load-more')
-let query = ''
-let page = 1
-let simpleLightBox
-const perPage = 40
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.btn-load-more');
+const endCollectionText = document.querySelector('.end-collection-text');
+let query = '';
+let page = 1;
+let currentHits = 0;
+// let simpleLightBox;
+const perPage = 40;
 
-searchForm.addEventListener('submit', onSearchForm)
-loadMoreBtn.addEventListener('click', onLoadMoreBtn)
+let lightbox = new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-onScroll()
-onToTopBtn()
+searchForm.addEventListener('submit', onSearchForm);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+onScroll();
+onToTopBtn();
+
 
 function onSearchForm(e) {
-  e.preventDefault()
-  window.scrollTo({ top: 0 })
-  page = 1
-  query = e.currentTarget.searchQuery.value.trim()
-  gallery.innerHTML = ''
-  loadMoreBtn.classList.add('is-hidden')
+  e.preventDefault();
+  window.scrollTo({ top: 0 });
+  page = 1;
+  query = e.currentTarget.searchQuery.value;
+  gallery.innerHTML = '';
+  loadMoreBtn.classList.add('is-hidden');
 
   if (query === '') {
-    alertNoEmptySearch()
-    return
+    alertNoEmptySearch();
+    return;
   }
 
   fetchImages(query, page, perPage)
     .then(({ data }) => {
       if (data.totalHits === 0) {
         alertNoImagesFound()
-      } else {
+      } 
+      else {
         renderGallery(data.hits)
-        simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+        lightbox.refresh();
         alertImagesFound(data)
 
         if (data.totalHits > perPage) {
@@ -49,25 +59,36 @@ function onSearchForm(e) {
     })
     .catch(error => console.log(error))
 }
+// let lightbox = new SimpleLightbox('.gallery a')
+  
+async function onLoadMoreBtn() {
+  page += 1;
+  const response = await fetchImages(query, page, perPage);
+  renderGallery(response.data.hits);
+  lightbox.refresh();
+  currentHits += response.hits.length;
 
-function onLoadMoreBtn() {
-  page += 1
-  simpleLightBox.destroy()
-
-  fetchImages(query, page, perPage)
-    .then(({ data }) => {
-      renderGallery(data.hits)
-      simpleLightBox = new SimpleLightbox('.gallery a').refresh()
-
-      const totalPages = Math.ceil(data.totalHits / perPage)
-
-      if (page > totalPages) {
-        loadMoreBtn.classList.add('is-hidden')
-        alertEndOfSearch()
-      }
-    })
-    .catch(error => console.log(error))
+  if (currentHits === response.totalHits) {
+    loadMoreBtn.classList.add('is-hidden');
+    endCollectionText.classList.remove('is-hidden');
+  }
 }
+
+
+//   fetchImages(query, page, perPage)
+//     .then(({ data }) => {
+//       renderGallery(data.hits)
+//       simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+
+//       const totalPages = Math.ceil(data.totalHits / perPage)
+
+//       if (page > totalPages) {
+//         loadMoreBtn.classList.add('is-hidden')
+//         alertEndOfSearch()
+//       }
+//     })
+//     .catch(error => console.log(error))
+// }
 
 function alertImagesFound(data) {
   Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
